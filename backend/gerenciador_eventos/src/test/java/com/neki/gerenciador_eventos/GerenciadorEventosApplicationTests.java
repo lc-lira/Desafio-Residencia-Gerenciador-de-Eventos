01a8d892ccae68690f801e24f9207df47fc6326d
lc-lira <lbrancolira@gmail.com>
2026-08-08 20:24:29 -0300

    configuração inicial do projeto

---
+++ backend/gerenciador_eventos/src/test/java/com/neki/gerenciador_eventos/GerenciadorEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neki.gerenciador_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
